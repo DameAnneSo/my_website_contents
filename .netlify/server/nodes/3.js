6321bc22fadeca93f561d1862307e98deb0261ee
DameAnneSo <asophiepereira@gmail.com>
2024-05-14 10:57:24 +0100

    change description of dream job

--- .netlify/server/nodes/3.js
+++ .netlify/server/nodes/3.js
@@ -5,6 +5,6 @@ let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/about/_page.svelte.js')).default;
 export { universal };
 export const universal_id = "src/routes/about/+page.js";
-export const imports = ["_app/immutable/nodes/3.7thIGP2H.js","_app/immutable/chunks/scheduler.tKmLD9AE.js","_app/immutable/chunks/index.aL9-j8ZR.js","_app/immutable/chunks/Footer_classic.sc-4yIQT.js"];
-export const stylesheets = ["_app/immutable/assets/3.lRVmWGcD.css","_app/immutable/assets/Footer_classic.q__GnokP.css"];
+export const imports = ["_app/immutable/nodes/3.4ER4Isbj.js","_app/immutable/chunks/scheduler.tKmLD9AE.js","_app/immutable/chunks/index.aL9-j8ZR.js","_app/immutable/chunks/Footer_classic.sc-4yIQT.js"];
+export const stylesheets = ["_app/immutable/assets/3.ncsxVz1F.css","_app/immutable/assets/Footer_classic.q__GnokP.css"];
 export const fonts = [];
