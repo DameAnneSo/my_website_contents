7493c43004f43c6f90210f9f17e13e4fcd12da97
DameAnneSo <asophiepereira@gmail.com>
2025-10-16 14:36:11 +0100

    adding background gradient colour feat to the project cat

--- .netlify/server/chunks/internal.js
+++ .netlify/server/chunks/internal.js
@@ -653,7 +653,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "6bt3fb"
+  version_hash: "qrsb8a"
 };
 async function get_hooks() {
   return {};
--- build/_app/immutable/entry/start.CX-cIDcW.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.xW-sMaPi.js";export{t as start};
